cb47a7aa2f144786c2b941056190332a099c662e
Braden Nicholson <braden@bradencnicholson.com>
2018-05-04 20:56:39 -0700

    House cleaning
    Adding explanation to mongo.php

--- mongo.php
+++ mongo.php
@@ -1,13 +1,16 @@
 <?php
+
+//Here we allow our mistakes to be known
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
+
+//Require the credentials to the db and security
 require("config.php");
+
 // include Composer's autoloader
 require("vendor/autoload.php");
+
+//Create mongodb client
 $client = new MongoDB\Client($cleveHomeMongoAddress);
-
-
-
-
 
 ?>
